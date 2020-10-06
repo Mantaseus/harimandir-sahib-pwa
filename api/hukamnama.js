4700267f88d48165c81c56175566e1ba1eac61ee
Satnam Singh Brar <brar.satnam1995@gmail.com>
2020-10-05 19:54:10 -0700

    Implement hukamnama parsing mechanism
    - We can now parse the raw hukamnama HTML and extract the relevant data
      out of it correctly
    - So, far it seems to be working well with various test values

--- api/hukamnama.js
+++ api/hukamnama.js
@@ -3,55 +3,81 @@ import http from 'http';
 import {StringDecoder} from 'string_decoder';
 
 function parseMukhvaak(html){
-    const fontElements = html.querySelectorAll('font');
-    const fontElementsText = []
+    /*
+    tableElements is going to have the following order and has the "metadata" about the hukamnama
+    [
+        // This is always there
+        "HUKAMNAMA\nFROM SRI DARBAR SAHIB,\n\n\nSri Amritsar.",
 
-    // FIrst section starts at index 1
-    const sectionStartIndexes = [2];
+        // 1 or more titles for the main hukamna
+        "iqlýg mhlw 4 ]",
 
-    // Loops through the '<font>' elements and identify the separators. Also generate an array
-    // of text from the font elements
-    for (var i in fontElements){
-        //const elementText = parse(fontElements[i].toString()).rawText.replace(/[\r\n\t]/g, '');;
-        const elementText = fontElements[i].rawText.replace(/[\r\n\t]/g, '');
-        fontElementsText.push(elementText);
+        // Date of the hukamna in punjabi
+        "somvwr, 20 A`sU (sMmq 552 nwnkSwhI)",
 
-        // Test if the element text has the separator element between mukhvaak and punjabi
+        // Page number of the hukamnama in punjabi. This serves as the indicator of where the main
+        // hukamnama section and punjabi translation sections are separated
+        "(AMg: 725)", 
+
+        // 1 or more titles for the punjabi translation section. Always same as the titles for the
+        // main hukamnam
+        "iqlýg mhlw 4 ]",
+
+        // This is always there. Marks the separation between the main hukamnam and the punjabi
         // translation
-        if (/pMjwbI ivAwiKAw/g.test(elementText)){
-            sectionStartIndexes.push(parseInt(i)+1);
-            continue;
-        }
+        "English\nTranslation :",
 
-        // Test if the element text has the separator element between punjabi translation and
-        // english translation
-        if (/English\s+Translation/g.test(elementText)){
-            sectionStartIndexes.push(parseInt(i)+1);
-            continue;
-        }
-    }
+        // English translation of the one or more titles. The number of titles here should be the
+        // same as the number of titles in the other sections
+        "TILANG, FOURTH MEHL:",
 
-    // Get the required data
-    const result = {
+        // English readable nanakshahi date of the hukamnam
+        "Monday, 20th Assu (Samvat 552 Nanakshahi)",
+
+        // English readable page number of the hukamnama
+        "(Page: 725)"
+    ]
+    */
+    const tableElements = html.querySelectorAll('td font')
+        .map(el => el.rawText.trim());
+
+    /*
+        paraElement should have 3 elements.
+        - First one for the body of the main hukamnama
+        - Second one for the body of the punjabi translation
+        - Third one for the body of the english translation
+    */
+    const paraElements = html.querySelectorAll('p font')
+        .map(el => el.rawText.trim())
+
+    return {
         mukhvaak: {
-            titles: fontElementsText.slice(sectionStartIndexes[0], sectionStartIndexes[1]-4),
-            body: fontElementsText[sectionStartIndexes[1]-4]
+            titles: tableElements.slice(
+                1,
+                tableElements.findIndex(el => el.startsWith('(AMg:')) - 1
+            ),
+            body: paraElements[0],
         },
         punjabiTranslation: {
-            titles: fontElementsText.slice(sectionStartIndexes[1], sectionStartIndexes[2]-2),
-            body: fontElementsText[sectionStartIndexes[2]-2],
-            punjabiDate: fontElementsText[sectionStartIndexes[1]-2],
-            page: fontElementsText[sectionStartIndexes[1]-1],
+            titles: tableElements.slice(
+                tableElements.findIndex(el => el.startsWith('(AMg:')) + 1,
+                tableElements.findIndex(el => /English\s+Translation/g.test(el))
+            ),
+            body: paraElements[1],
         },
         englishTranslation: {
-            titles: fontElementsText.slice(sectionStartIndexes[2], fontElementsText.length-3),
-            body: fontElementsText[fontElementsText.length-3],
-            punjabiDate: fontElementsText[fontElementsText.length-2],
-            page: fontElementsText[fontElementsText.length-1],
-        }
-    }
-
-    return result;
+            titles: tableElements.slice(
+                tableElements.findIndex(el => /English\s+Translation/g.test(el)) + 1,
+                -2
+            ),
+            body: paraElements[2],
+        },
+        date: {
+            punjabi: tableElements[tableElements.findIndex(el => el.startsWith('(AMg:')) - 1],
+            english: tableElements[tableElements.length - 2],
+        },
+        pageNumber: Number(tableElements[tableElements.length - 1].match(/\d+/)),
+    };
 }
 
 module.exports = (request, response) => {
