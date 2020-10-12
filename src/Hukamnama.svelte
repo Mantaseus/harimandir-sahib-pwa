21b84801829bef153c37227c7bea69043c3f27f2
Satnam Singh Brar <brar.satnam1995@gmail.com>
2020-10-12 16:17:48 -0700

    Add separator between hukamnama sections

--- src/Hukamnama.svelte
+++ src/Hukamnama.svelte
@@ -9,12 +9,13 @@
     onMount(async () => {
         hukamnama = await getHukamnama();
     })
+
     async function getHukamnama() {
         const day = date.getDate();
         const month = date.getMonth() + 1;
         const year = date.getFullYear();
 
-        return await fetch(`/api/hukamnama?day=${day}&month=${month}&year=${year}`, {
+        return fetch(`/api/hukamnama?day=${day}&month=${month}&year=${year}`, {
             method: 'GET',
         }).then(res => res.json())
     }
@@ -37,6 +38,16 @@
     .justified {
         text-align: justify;
     }
+
+    .separator {
+        color: #5d5400;
+        background-color: #e2cc00;
+        width: 100%;
+        border-radius: 1em;
+        box-sizing: border-box;
+        margin: 1em 0 0.5em 0;
+        padding: 0.1em 0
+    }
 </style>
 
 {#if !_.isEmpty(hukamnama)}
@@ -45,11 +56,13 @@
     {/each}
     <div class="justified punjabi">{hukamnama.mukhvaak.body}</div>
 
+    <div class="punjabi centered separator">ArQ</div>
     {#each hukamnama.punjabiTranslation.titles as title}
         <h3 class="centered punjabi">{title}</h3>
     {/each}
     <div class="justified punjabi">{hukamnama.punjabiTranslation.body}</div>
 
+    <div class="centered separator">Translation</div>
     {#each hukamnama.englishTranslation.titles as title}
         <h3 class="centered">{title}</h3>
     {/each}
