a87ceee0eefbe1ce5fc4aeb6a1681a2f5140c883
Satnam Singh Brar <brar.satnam1995@gmail.com>
2022-06-29 08:26:28 -0700

    feat: The `hukamnama` api now just proxies the SGPC page
    - The error that was observed on the live app seemed to be coming from
      the conversion of some data from the SGPC page
    - So, I decided to remove the conversion process altogether from the
      serverless function. The conversion will now happen in the frontend
      where it will be easier to debug the issue
    - The serverless function is now just a proxy for the SGPC page that we
      need to keep because we can't fetch the SGPC page directly on the
      frontend because of CORS permission issues from the SGPC page server

--- api/hukamnama.js
+++ api/hukamnama.js
@@ -1,114 +1,89 @@
-import {parse} from 'node-html-parser';
 import http from 'http';
-import iconv from 'iconv-lite';
 
 function parseMukhvaak(html){
-    /*
-    tableElements is going to have the following order and has the "metadata" about the hukamnama
-    [
-        // This is always there
-        "HUKAMNAMA\nFROM SRI DARBAR SAHIB,\n\n\nSri Amritsar.",
+  /*
+  tableElements is going to have the following order and has the "metadata" about the hukamnama
+  [
+      // This is always there
+      "HUKAMNAMA\nFROM SRI DARBAR SAHIB,\n\n\nSri Amritsar.",
 
-        // 1 or more titles for the main hukamna
-        "iqlýg mhlw 4 ]",
+      // 1 or more titles for the main hukamna
+      "iqlýg mhlw 4 ]",
 
-        // Date of the hukamna in punjabi
-        "somvwr, 20 A`sU (sMmq 552 nwnkSwhI)",
+      // Date of the hukamna in punjabi
+      "somvwr, 20 A`sU (sMmq 552 nwnkSwhI)",
 
-        // Page number of the hukamnama in punjabi. This serves as the indicator of where the main
-        // hukamnama section and punjabi translation sections are separated
-        "(AMg: 725)", 
+      // Page number of the hukamnama in punjabi. This serves as the indicator of where the main
+      // hukamnama section and punjabi translation sections are separated
+      "(AMg: 725)", 
 
-        // 1 or more titles for the punjabi translation section. Always same as the titles for the
-        // main hukamnam
-        "iqlýg mhlw 4 ]",
+      // 1 or more titles for the punjabi translation section. Always same as the titles for the
+      // main hukamnam
+      "iqlýg mhlw 4 ]",
 
-        // This is always there. Marks the separation between the main hukamnam and the punjabi
-        // translation
-        "English\nTranslation :",
+      // This is always there. Marks the separation between the main hukamnam and the punjabi
+      // translation
+      "English\nTranslation :",
 
-        // English translation of the one or more titles. The number of titles here should be the
-        // same as the number of titles in the other sections
-        "TILANG, FOURTH MEHL:",
+      // English translation of the one or more titles. The number of titles here should be the
+      // same as the number of titles in the other sections
+      "TILANG, FOURTH MEHL:",
 
-        // English readable nanakshahi date of the hukamnam
-        "Monday, 20th Assu (Samvat 552 Nanakshahi)",
+      // English readable nanakshahi date of the hukamnam
+      "Monday, 20th Assu (Samvat 552 Nanakshahi)",
 
-        // English readable page number of the hukamnama
-        "(Page: 725)"
-    ]
-    */
-    const tableElements = html.querySelectorAll('td font')
-        .map(el => el.rawText.trim());
+      // English readable page number of the hukamnama
+      "(Page: 725)"
+  ]
+  */
+  const tableElements = html.querySelectorAll('td font')
+    .map(el => el.rawText.trim());
 
-    /*
-        paraElement should have 3 elements.
-        - First one for the body of the main hukamnama
-        - Second one for the body of the punjabi translation
-        - Third one for the body of the english translation
-    */
-    const paraElements = html.querySelectorAll('p font')
-        .map(el => el.rawText.trim())
+  /*
+      paraElement should have 3 elements.
+      - First one for the body of the main hukamnama
+      - Second one for the body of the punjabi translation
+      - Third one for the body of the english translation
+  */
+  const paraElements = html.querySelectorAll('p font')
+      .map(el => el.rawText.trim())
 
-    const mukhvaakIndex = 1;
-    const punjabiTranslationIndex = tableElements.findIndex(el => /^\(?AMg/ig.test(el));
-    const englishTranslationIndex = tableElements.findIndex(el => /English\s+Translation/ig.test(el));
+  const mukhvaakIndex = 1;
+  const punjabiTranslationIndex = tableElements.findIndex(el => /^\(?AMg/ig.test(el));
+  const englishTranslationIndex = tableElements.findIndex(el => /English\s+Translation/ig.test(el));
 
-    return {
-        mukhvaak: {
-            titles: tableElements.slice(mukhvaakIndex, punjabiTranslationIndex - 1),
-            body: paraElements[0],
-        },
-        punjabiTranslation: {
-            titles: tableElements.slice(punjabiTranslationIndex + 1, englishTranslationIndex),
-            body: paraElements[1],
-        },
-        englishTranslation: {
-            titles: tableElements.slice(englishTranslationIndex + 1, -2),
-            body: paraElements[2],
-        },
-        date: {
-            punjabi: tableElements[punjabiTranslationIndex - 1],
-            english: tableElements[tableElements.length - 2],
-        },
-        pageNumber: Number(tableElements[tableElements.length - 1].match(/\d+/)),
-    };
+  return {
+    mukhvaak: {
+      titles: tableElements.slice(mukhvaakIndex, punjabiTranslationIndex - 1),
+      body: paraElements[0],
+    },
+    punjabiTranslation: {
+      titles: tableElements.slice(punjabiTranslationIndex + 1, englishTranslationIndex),
+      body: paraElements[1],
+    },
+    englishTranslation: {
+      titles: tableElements.slice(englishTranslationIndex + 1, -2),
+      body: paraElements[2],
+    },
+    date: {
+      punjabi: tableElements[punjabiTranslationIndex - 1],
+      english: tableElements[tableElements.length - 2],
+    },
+    pageNumber: Number(tableElements[tableElements.length - 1].match(/\d+/)),
+  };
 }
 
 module.exports = (request, response) => {
-    const {query: {day, month, year}} = request;
+  const { query: {day, month, year} } = request;
 
-    const options = {
-        hostname: 'old.sgpc.net',
-        path: `/hukumnama/oldhukumnama.asp?print_flag=Y&nullstr_flag=&date1=${day}&month=${month}&year=${year}&kk=2`,
-        method: 'GET',
-    }
-    console.log(options)
+  const options = {
+    hostname: 'old.sgpc.net',
+    path: `/hukumnama/oldhukumnama.asp?print_flag=Y&nullstr_flag=&date1=${day}&month=${month}&year=${year}&kk=2`,
+    method: 'GET',
+  }
+  console.log(options);
 
-    const req = http.request(options, res => {
-        res.pipe(iconv.decodeStream('windows-1252')).collect((err, data) => {
-            if(data) {
-                try {
-                    response.json(parseMukhvaak(parse(
-                        // Sometimes, an HTML response may have a weird email protection link thing
-                        // embedded in the text. So, this tries to remove it
-                        data.replace(/\[email.*protected\]/, '')
-                    )));
-                } catch (e) {
-                    response.json({
-                        error: true,
-                        reason: `Failed to parce hukamnama from source: ${e}`,
-                    })
-                }
-            } else {
-                response.json({
-                    error: true,
-                    reason: 'No response from hukamnama source'
-                })
-            }
-        })
-    })
-
-    req.on('error', console.error);
-    req.end();
-}
+  http.request(options, res => res.pipe(response, { end: true }))
+    .on('error', console.error)
+    .end();
+};
